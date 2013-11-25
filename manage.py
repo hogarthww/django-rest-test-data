b8dcd756b985259dfd8e88a716c98e39a8bba89e
Daniel Watkins <daniel.watkins@hogarthww.com>
2013-11-25 10:32:35 +0000

    Fix manage.py with empty DJANGO_SETTINGS_MODULE env variable.

--- manage.py
+++ manage.py
@@ -3,7 +3,9 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testsettings")
+    settings_module = os.environ.get('DJANGO_SETTINGS_MODULE')
+    if not settings_module:
+        os.environ['DJANGO_SETTINGS_MODULE'] = 'testsettings'
 
     from django.core.management import execute_from_command_line
 
